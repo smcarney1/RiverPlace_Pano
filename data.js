aced5dc75b0d9d964f73dce92b529ff512b272a4
smcarney1 <smcarney1@gmail.com>
2025-05-30 14:54:26 -0600

    panos

---
+++ data.js
@@ -0,0 +1,779 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-riverplace_pano",
+      "name": "RiverPlace_Pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.3482703203993527,
+        "pitch": 0.6980527272546997,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6939520662251084,
+          "pitch": 0.13362301224225348,
+          "rotation": 0,
+          "target": "1-riverplace_pano-1"
+        },
+        {
+          "yaw": 0.4239802644853228,
+          "pitch": 0.15531453989267874,
+          "rotation": 0,
+          "target": "2-riverplace_pano-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-riverplace_pano-1",
+      "name": "RiverPlace_Pano-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.9106878704156056,
+        "pitch": 0.6368188148544789,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5478928731870703,
+          "pitch": 0.11529726013964847,
+          "rotation": 0,
+          "target": "0-riverplace_pano"
+        },
+        {
+          "yaw": 0.6008899963796317,
+          "pitch": 0.20281812500624596,
+          "rotation": 0,
+          "target": "2-riverplace_pano-2"
+        },
+        {
+          "yaw": -0.8940971877550261,
+          "pitch": 0.03624922793040497,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": -0.39934517340863884,
+          "pitch": 0.03677293127483239,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-riverplace_pano-2",
+      "name": "RiverPlace_Pano-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 2.9059580291451574,
+        "pitch": 0.6662019895497409,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.640199692214283,
+          "pitch": 0.10615952829651576,
+          "rotation": 0,
+          "target": "0-riverplace_pano"
+        },
+        {
+          "yaw": -2.389148490039055,
+          "pitch": 0.12972792566216285,
+          "rotation": 0,
+          "target": "1-riverplace_pano-1"
+        },
+        {
+          "yaw": 0.725107006676426,
+          "pitch": 0.1533707734927603,
+          "rotation": 0,
+          "target": "3-riverplace_pano-03"
+        },
+        {
+          "yaw": -0.7833665347004164,
+          "pitch": 0.036178624960975014,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": -0.43011057865792424,
+          "pitch": 0.014941693847793758,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        },
+        {
+          "yaw": 0.4866210372288222,
+          "pitch": 0.07593171731199533,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-riverplace_pano-03",
+      "name": "RiverPlace_Pano-03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.13837967640812998,
+        "pitch": 0.6637397799294416,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05164842298643535,
+          "pitch": 0.14580579080286427,
+          "rotation": 0,
+          "target": "2-riverplace_pano-2"
+        },
+        {
+          "yaw": -0.7175571955199835,
+          "pitch": 0.05194175841174342,
+          "rotation": 0,
+          "target": "0-riverplace_pano"
+        },
+        {
+          "yaw": 2.904517948525517,
+          "pitch": 0.10776005970164171,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": 2.1287421936705018,
+          "pitch": 0.045083977654606144,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        },
+        {
+          "yaw": 1.3709092700109764,
+          "pitch": 0.005130004948306066,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": 1.5892846718656495,
+          "pitch": 0.02125192527137898,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-riverplace_pano-04",
+      "name": "RiverPlace_Pano-04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.8452380234658534,
+        "pitch": 0.6368188148544753,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08184677706404031,
+          "pitch": 0.07304476362287637,
+          "rotation": 0,
+          "target": "6-riverplace_pano-06"
+        },
+        {
+          "yaw": 0.39215238631638627,
+          "pitch": 0.021519855935999033,
+          "rotation": 0,
+          "target": "5-riverplace_pano-05"
+        },
+        {
+          "yaw": -0.8037749583631122,
+          "pitch": 0.04105739907436856,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        },
+        {
+          "yaw": -1.1262263884552315,
+          "pitch": 0.022415505656624646,
+          "rotation": 0,
+          "target": "9-riverplace_pano-09"
+        },
+        {
+          "yaw": -2.8874127235736218,
+          "pitch": 0.06989604727250054,
+          "rotation": 0,
+          "target": "3-riverplace_pano-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-riverplace_pano-05",
+      "name": "RiverPlace_Pano-05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.5740886575309876,
+        "pitch": 0.6686604287847366,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2710051790755674,
+          "pitch": 0.38166642148389585,
+          "rotation": 0,
+          "target": "6-riverplace_pano-06"
+        },
+        {
+          "yaw": 0.06945741079609391,
+          "pitch": 0.08688047844777813,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": 1.3483610151166285,
+          "pitch": 0.09873627387768558,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-riverplace_pano-06",
+      "name": "RiverPlace_Pano-06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.970904449897981,
+        "pitch": 0.7005006963399278,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48401004830713745,
+          "pitch": 0.11982354255662919,
+          "rotation": 0,
+          "target": "5-riverplace_pano-05"
+        },
+        {
+          "yaw": 1.2432918846441936,
+          "pitch": 0.10143121635853092,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": 3.028258356490208,
+          "pitch": 0.13332026783988127,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        },
+        {
+          "yaw": 2.825163224668235,
+          "pitch": 0.0070045401175384825,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-riverplace_pano-07",
+      "name": "RiverPlace_Pano-07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.42075369747833946,
+        "pitch": 0.7103101721138856,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8328727035790138,
+          "pitch": 0.06942869335520996,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": -1.7013363846863765,
+          "pitch": 0.10655380785404489,
+          "rotation": 0,
+          "target": "6-riverplace_pano-06"
+        },
+        {
+          "yaw": 1.563975510992666,
+          "pitch": 0.16006965633097003,
+          "rotation": 0,
+          "target": "8-riverplace_pano-08"
+        },
+        {
+          "yaw": 0.969775198860491,
+          "pitch": 0.1491126017790787,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-riverplace_pano-08",
+      "name": "RiverPlace_Pano-08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.8527264981972422,
+        "pitch": 0.7005006963399207,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.009379439518463428,
+          "pitch": 0.24092133333670596,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        },
+        {
+          "yaw": 0.5614625971010305,
+          "pitch": 0.06135481963048761,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": -3.135673296611788,
+          "pitch": 0.15436681278620412,
+          "rotation": 0,
+          "target": "9-riverplace_pano-09"
+        },
+        {
+          "yaw": 2.5857063682174317,
+          "pitch": 0.04536714259384311,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": 1.3631864202112265,
+          "pitch": 0.0013264213124664082,
+          "rotation": 0,
+          "target": "1-riverplace_pano-1"
+        },
+        {
+          "yaw": 1.8298554229453625,
+          "pitch": 0.2799057113132104,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-riverplace_pano-09",
+      "name": "RiverPlace_Pano-09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.8097081065502252,
+        "pitch": 0.6784569681334176,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.002795142750017021,
+          "pitch": 0.1799621350726568,
+          "rotation": 0,
+          "target": "8-riverplace_pano-08"
+        },
+        {
+          "yaw": -0.020637898999520843,
+          "pitch": 0.05718942800907634,
+          "rotation": 0,
+          "target": "6-riverplace_pano-06"
+        },
+        {
+          "yaw": 2.2391490279421875,
+          "pitch": 0.09572382190443385,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": 1.1561740197706083,
+          "pitch": -0.00043843400768750485,
+          "rotation": 0,
+          "target": "1-riverplace_pano-1"
+        },
+        {
+          "yaw": 0.6770178713045603,
+          "pitch": 0.217056200285338,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-riverplace_pano-10",
+      "name": "RiverPlace_Pano-10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.6769384170235266,
+        "pitch": 0.7078486057420932,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0326438542781045,
+          "pitch": 0.09363188034456726,
+          "rotation": 0,
+          "target": "9-riverplace_pano-09"
+        },
+        {
+          "yaw": -1.6742590702312032,
+          "pitch": 0.06258709886179759,
+          "rotation": 0,
+          "target": "8-riverplace_pano-08"
+        },
+        {
+          "yaw": -0.9238937115577244,
+          "pitch": 0.03154384626645168,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": -0.24097014276161666,
+          "pitch": 0.018732061000513056,
+          "rotation": 0,
+          "target": "1-riverplace_pano-1"
+        },
+        {
+          "yaw": -1.4490062802551034,
+          "pitch": 0.11884208149440134,
+          "rotation": 0,
+          "target": "11-riverplace_pano-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-riverplace_pano-11",
+      "name": "RiverPlace_Pano-11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.2575375542696072,
+        "pitch": 0.7134976756777789,
+        "fov": 1.5463268116295634
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2436436676345686,
+          "pitch": 0.08556653665720937,
+          "rotation": 0,
+          "target": "10-riverplace_pano-10"
+        },
+        {
+          "yaw": -3.025131366060375,
+          "pitch": 0.16214991800020861,
+          "rotation": 0,
+          "target": "9-riverplace_pano-09"
+        },
+        {
+          "yaw": -1.814332777159148,
+          "pitch": 0.24432928457918024,
+          "rotation": 0,
+          "target": "8-riverplace_pano-08"
+        },
+        {
+          "yaw": -1.242883222071864,
+          "pitch": 0.1407982038351676,
+          "rotation": 0,
+          "target": "7-riverplace_pano-07"
+        },
+        {
+          "yaw": -0.3280413217589473,
+          "pitch": 0.07189522769944645,
+          "rotation": 0,
+          "target": "4-riverplace_pano-04"
+        },
+        {
+          "yaw": 0.2732855318264171,
+          "pitch": 0.053542810241147265,
+          "rotation": 0,
+          "target": "2-riverplace_pano-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "River Place Panos",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
